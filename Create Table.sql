a0938765ea5c3e7bde9a36d22e024775e763333f
hoangtheanhHE160176 <anhhthe160176@fpt.edu.vn>
2022-07-13 17:44:08 +0700

    Update Create Table.sql

--- Create Table.sql
+++ Create Table.sql
@@ -1,38 +1,52 @@
 CREATE TABLE Student(
-	sid varchar(50) PRIMARY KEY NOT NULL,
-	name nvarchar(150) NOT NULL,
-	dob date NOT NULL,
-	address nvarchar(150) NOT NULL,
+	StudentID varchar(50) PRIMARY KEY NOT NULL,
+	Name nvarchar(150) NOT NULL,
+	Dob date NOT NULL,
+	Address nvarchar(150) NOT NULL,
+	Gender bit NOT NULL
 	)
 
 
-CREATE TABLE Group_Student(
-	gid varchar(50) PRIMARY KEY NOT NULL,
-	name nvarchar(150) NOT NULL
+CREATE TABLE Subject(
+	SubjectID int PRIMARY KEY NOT NULL,
+	SubjectName nvarchar(150) NOT NULL
 )
-
 
 CREATE TABLE Lecture(
-	lid varchar(50) PRIMARY KEY NOT NULL,
-	name nvarchar(150) NOT NULL,
-	email varchar(50) NOT NULL
+	LectureID int PRIMARY KEY NOT NULL,
+	Name nvarchar(150) NOT NULL,
+	Email varchar(50) NOT NULL
 )
 
+CREATE TABLE [Group](
+	GroupID varchar(50) PRIMARY KEY NOT NULL,
+	Name nvarchar(150) NOT NULL,
+	SubjectID int NOT NULL
+	FOREIGN KEY (SubjectID) REFERENCES [Subject] (SubjectID),
+	LectureID int NOT NULL
+	FOREIGN KEY (LectureID) REFERENCES [Lecture] (LectureID),
+)
 
-CREATE TABLE Course(
-	cid int PRIMARY KEY NOT NULL,
-	name nvarchar(150) NOT NULL
+CREATE TABLE Semester(
+	SemesterID int PRIMARY KEY NOT NULL,
+	Name nvarchar(150) NOT NULL
 )
 
 
 CREATE TABLE Assessment(
-	aid int PRIMARY KEY NOT NULL,
-	name nvarchar(150) NOT NULL,
-	weight float NOT NULL,
-	type nvarchar(150) NOT NULL
+	AssessmentID int PRIMARY KEY NOT NULL,
+	SubjectID int NOT NULL
+	FOREIGN KEY (SubjectID) REFERENCES [Subject] (SubjectID),
 )
 
+CREATE TABLE Assessment_Info(
+	CategoryID int PRIMARY KEY NOT NULL,
+	CategoryName nvarchar(150) NOT NULL,
+	Weight float NOT NULL,
+	Part int NOT NULL,
+	Type nvarchar(50) NOT NULL
 
+)
 CREATE TABLE Result(
 	item nvarchar(150) PRIMARY KEY NOT NULL,
 	comment nvarchar(50),
