c3d7755cf0f66002bf80b8242c362c8dae602089
hoangtheanhHE160176 <anhhthe160176@fpt.edu.vn>
2022-07-14 17:37:51 +0700

    Update ASM

--- Create Table.sql
+++ Create Table.sql
@@ -1,11 +1,10 @@
 CREATE TABLE Student(
-	StudentID varchar(50) PRIMARY KEY NOT NULL,
+	StudentID int PRIMARY KEY NOT NULL,
 	Name nvarchar(150) NOT NULL,
 	Dob date NOT NULL,
 	Address nvarchar(150) NOT NULL,
 	Gender bit NOT NULL
 	)
-
 
 CREATE TABLE Subject(
 	SubjectID int PRIMARY KEY NOT NULL,
@@ -17,28 +16,34 @@ CREATE TABLE Lecture(
 	Name nvarchar(150) NOT NULL,
 	Email varchar(50) NOT NULL
 )
-
 CREATE TABLE [Group](
 	GroupID varchar(50) PRIMARY KEY NOT NULL,
 	Name nvarchar(150) NOT NULL,
 	SubjectID int NOT NULL
-	FOREIGN KEY (SubjectID) REFERENCES [Subject] (SubjectID),
+	FOREIGN KEY (SubjectID) REFERENCES Subject(SubjectID),
 	LectureID int NOT NULL
-	FOREIGN KEY (LectureID) REFERENCES [Lecture] (LectureID),
+	FOREIGN KEY (LectureID) REFERENCES Lecture (LectureID),
 )
 
-CREATE TABLE Semester(
-	SemesterID int PRIMARY KEY NOT NULL,
-	Name nvarchar(150) NOT NULL
+CREATE TABLE Group_Student(
+	StudentID int NOT NULL,
+	GroupID varchar(50) NOT NULL,
+	PRIMARY KEY(StudentID, GroupID),
+	FOREIGN KEY (StudentID) REFERENCES Student (StudentID),
+	FOREIGN KEY (GroupID) REFERENCES [Group] (GroupID),
 )
 
 
-CREATE TABLE Assessment(
-	AssessmentID int PRIMARY KEY NOT NULL,
-	SubjectID int NOT NULL
+
+CREATE TABLE Subject_Semester(
+	SubjectSemesterID int PRIMARY KEY NOT NULL,
+	SubjectID int NOT NULL 
 	FOREIGN KEY (SubjectID) REFERENCES [Subject] (SubjectID),
-)
+	Semester nvarchar(150) NOT NULL,
+	StartDate date NOT NULL,
+	EndDate date NOT NULL
 
+)
 CREATE TABLE Assessment_Info(
 	CategoryID int PRIMARY KEY NOT NULL,
 	CategoryName nvarchar(150) NOT NULL,
@@ -47,9 +52,22 @@ CREATE TABLE Assessment_Info(
 	Type nvarchar(50) NOT NULL
 
 )
-CREATE TABLE Result(
-	item nvarchar(150) PRIMARY KEY NOT NULL,
-	comment nvarchar(50),
-	weight float NOT NULL,
-	value float NOT NULL
+
+
+CREATE TABLE Assessment(
+	AssessmentID int PRIMARY KEY NOT NULL,
+	SubjectID int NOT NULL,
+	CategoryID int NOT NULL
+	FOREIGN KEY (SubjectID) REFERENCES [Subject] (SubjectID),
+	FOREIGN KEY (CategoryID) REFERENCES Assessment_Info (CategoryID)
+)
+
+
+CREATE TABLE Result_Table(
+	ResultTableID int PRIMARY KEY NOT NULL,
+	AssessmentID int NOT NULL
+	FOREIGN KEY (AssessmentID) REFERENCES Assessment (AssessmentID),
+	StudentID int NOT NULL,	
+	FOREIGN KEY (StudentID) REFERENCES Student (StudentID),
+	Result float NOT NULL,	
 )
